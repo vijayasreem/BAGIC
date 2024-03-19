d63570768f69de2f84fc6469d40945f124f59c47
root <root@b7aa676dfe3a>
2024-03-19 09:21:44 +0000

    pushed_from_larcas

--- src/main/java/com/BAGICLoan/repository/LoanApplicationRepository.java
+++ src/main/java/com/BAGICLoan/repository/LoanApplicationRepository.java
@@ -1,8 +1,8 @@
-
 package com.BAGICLoan.repository;
 
 import com.BAGICLoan.model.LoanApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -28,4 +28,9 @@ public interface LoanApplicationRepository extends JpaRepository<LoanApplication
     // Custom query to retrieve loan applications by applicant email
     @Query("SELECT l FROM LoanApplication l WHERE l.applicantEmail = ?1")
     List<LoanApplication> findByApplicantEmail(String applicantEmail);
+
+    // Custom query to update loan application
+    @Modifying
+    @Query("UPDATE LoanApplication l SET l.applicationType = ?1, l.approvalStatus = ?2, l.applicantId = ?3, l.applicantName = ?4, l.applicantEmail = ?5 WHERE l.id = ?6")
+    LoanApplication updateLoanApplication(String applicationType, String approvalStatus, Long applicantId, String applicantName, String applicantEmail, Long id);
 }
